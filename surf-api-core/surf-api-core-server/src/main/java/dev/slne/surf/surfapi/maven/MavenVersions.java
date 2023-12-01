707cb7b9cf49a54c2d24085edd6570b19136eb1d
twisti <twisti@slne.dev>
2023-12-01 18:29:27 +0100

    Add MavenVersions utility and update pom.xml
    A new utility class, MavenVersions, is introduced to manage versions of Maven artifacts. The file maven-versions.properties is created to store these versions. The pom.xml file is also updated to reflect these versions. Further modifications to pom.xml include adding project meta-data and dependencies.

---
+++ surf-api-core/surf-api-core-server/src/main/java/dev/slne/surf/surfapi/maven/MavenVersions.java
@@ -0,0 +1,79 @@
+package dev.slne.surf.surfapi.maven;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * A utility class providing access to the versions of Maven artifacts.
+ *
+ * <p>
+ * This class provides static final fields for commonly used Maven artifacts such as Guava, Caffeine, GSON, Commons Lang3, and Commons Text.
+ * The versions of these artifacts are loaded from a properties file named "maven-versions.properties".
+ * </p>
+ *
+ * <p>
+ * Usage:
+ * <pre>{@code
+ * String guavaVersion = MavenVersions.GUAVA_VERSION;
+ * String caffeineVersion = MavenVersions.CAFFEINE_VERSION;
+ * String gsonVersion = MavenVersions.GSON_VERSION;
+ * String lang3Version = MavenVersions.COMMONS_LANG3_VERSION;
+ * String textVersion = MavenVersions.COMMONS_TEXT_VERSION;
+ * }</pre>
+ * </p>
+ *
+ * <p>
+ * Note: The properties file "maven-versions.properties" must be present on the classpath and should contain the following keys:
+ * <ul>
+ *   <li>guava</li>
+ *   <li>caffeine</li>
+ *   <li>gson</li>
+ *   <li>commons-lang3</li>
+ *   <li>commons-text</li>
+ * </ul>
+ * </p>
+ */
+public final class MavenVersions {
+
+    /**
+     * The version of the Guava artifact.
+     */
+    public static final String GUAVA_VERSION;
+
+    /**
+     * The version of the Caffeine artifact.
+     */
+    public static final String CAFFEINE_VERSION;
+
+    /**
+     * The version of the Gson artifact.
+     */
+    public static final String GSON_VERSION;
+
+    /**
+     * The version of the Commons Lang3 artifact.
+     */
+    public static final String COMMONS_LANG3_VERSION;
+
+    /**
+     * The version of the Commons Text artifact.
+     */
+    public static final String COMMONS_TEXT_VERSION;
+
+    static {
+        // load properties file
+        Properties properties = new Properties();
+
+        try {
+            properties.load(MavenVersions.class.getResourceAsStream("/maven-versions.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        GUAVA_VERSION = properties.getProperty("guava");
+        CAFFEINE_VERSION = properties.getProperty("caffeine");
+        GSON_VERSION = properties.getProperty("gson");
+        COMMONS_LANG3_VERSION = properties.getProperty("commons-lang3");
+        COMMONS_TEXT_VERSION = properties.getProperty("commons-text");
+    }
+}
