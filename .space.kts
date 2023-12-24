2efad81734cb35544a58329a66341cd505373923
twisti <twisti@slne.dev>
2023-12-24 22:00:58 +0100

    feat: added functionality to .space.kts

--- .space.kts
+++ .space.kts
@@ -6,6 +6,10 @@ job("Build and publish") {
            val JB_SPACE_CLIENT_ID by env
            val JB_SPACE_CLIENT_SECRET by env
 
+           println("REPOSITORY_URL: $REPOSITORY_URL")
+              println("JB_SPACE_CLIENT_ID: $JB_SPACE_CLIENT_ID")
+                println("JB_SPACE_CLIENT_SECRET: $JB_SPACE_CLIENT_SECRET")
+
            api.gradlew("shadowJar", "--stacktrace")
            api.gradlew("publish", "--stacktrace", "-DrepoUrl=$REPOSITORY_URL", "-Dusername=$JB_SPACE_CLIENT_ID", "-Dpassword=$JB_SPACE_CLIENT_SECRET")
        }
