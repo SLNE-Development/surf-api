571e2db27e2f50dcba08f766fe77a58068437d2e
twisti <twisti@slne.dev>
2023-12-24 22:26:01 +0100

    feat: .space.kts cleanup [skip ci]

--- .space.kts
+++ .space.kts
@@ -1,16 +1,10 @@
 job("Build and publish") {
-   container("Run gradle build", "eclipse-temurin:18") {
-       env["REPOSITORY_URL"] = "https://packages.slne.dev/maven/p/surf/maven"
-       kotlinScript {api ->
-           val env = System.getenv()
-           val REPOSITORY_URL by env
-           val JB_SPACE_CLIENT_ID by env
-           val JB_SPACE_CLIENT_SECRET by env
+    container("Run gradle build", "eclipse-temurin:18") {
+        env["REPOSITORY_URL"] = "https://packages.slne.dev/maven/p/surf/maven"
 
-           api.gradlew("shadowJar", "--stacktrace")
-           api.gradlew("publish", "--stacktrace",
-//               "-DrepoUrl=$REPOSITORY_URL", "-Dusername=$JB_SPACE_CLIENT_ID", "-Dpassword=$JB_SPACE_CLIENT_SECRET"
-           )
-       }
-   }
+        kotlinScript { api ->
+            api.gradlew("shadowJar", "--stacktrace")
+            api.gradlew("publish", "--stacktrace")
+        }
+    }
 }
