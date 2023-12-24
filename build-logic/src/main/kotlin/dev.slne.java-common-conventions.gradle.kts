d18d16f6e933bd068a9ad22d2b2f2226de770f2d
twisti <twisti@slne.dev>
2023-12-24 22:15:00 +0100

    feat: added functionality to .space.kts [skip ci]

--- build-logic/src/main/kotlin/dev.slne.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/dev.slne.java-common-conventions.gradle.kts
@@ -39,7 +39,7 @@ publishing {
     repositories {
         maven {
             name = "slne-space"
-            url = uri(System.getenv("REPOSITORY_URL"))
+            url = uri(System.getenv("REPOSITORY_URL") ?: "https://packages.slne.dev/maven/p/surf/maven")
             credentials {
                 username = System.getenv("JB_SPACE_CLIENT_ID")
                 password = System.getenv("JB_SPACE_CLIENT_SECRET")
