5c12252bd17cf9b1e4636cedd4230aeabf320349
twisti <twisti@slne.dev>
2023-12-24 22:18:46 +0100

    feat: added functionality to .space.kts

--- build-logic/src/main/kotlin/dev.slne.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/dev.slne.java-common-conventions.gradle.kts
@@ -32,8 +32,6 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
         }
     }
     repositories {
