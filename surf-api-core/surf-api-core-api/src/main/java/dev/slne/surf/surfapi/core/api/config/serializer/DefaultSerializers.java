6332befb532d34ba8a946698a538ecfdee118238
twisti <twisti@slne.dev>
2024-02-29 20:59:31 +0100

    feat: added option to modify mini message used in configs

--- surf-api-core/surf-api-core-api/src/main/java/dev/slne/surf/surfapi/core/api/config/serializer/DefaultSerializers.java
+++ surf-api-core/surf-api-core-api/src/main/java/dev/slne/surf/surfapi/core/api/config/serializer/DefaultSerializers.java
@@ -2,9 +2,12 @@ package dev.slne.surf.surfapi.core.api.config.serializer;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Consumer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import space.arim.dazzleconf.error.BadValueException;
 import space.arim.dazzleconf.serialiser.Decomposer;
 import space.arim.dazzleconf.serialiser.FlexibleType;
@@ -23,19 +26,38 @@ public final class DefaultSerializers {
 
   public static final class ComponentSerializer implements ValueSerialiser<Component> {
 
+    private static final MiniMessage.Builder builder = MiniMessage.builder();
+    private static MiniMessage miniMessage = builder.build();
+    private static boolean modified = false;
+
+    @Contract(pure = true)
     @Override
     public Class<Component> getTargetClass() {
       return Component.class;
     }
 
     @Override
-    public Component deserialise(FlexibleType flexibleType) throws BadValueException {
-      return MiniMessage.miniMessage().deserialize(flexibleType.getString());
+    public @NotNull Component deserialise(@NotNull FlexibleType flexibleType) throws BadValueException {
+      return getMiniMessage().deserialize(flexibleType.getString());
     }
 
     @Override
-    public String serialise(Component value, Decomposer decomposer) {
-      return MiniMessage.miniMessage().serialize(value);
+    public @NotNull String serialise(Component value, Decomposer decomposer) {
+      return getMiniMessage().serialize(value);
+    }
+
+    private static MiniMessage getMiniMessage() {
+      if (modified) {
+        miniMessage = builder.build();
+        modified = false;
+      }
+
+      return miniMessage;
+    }
+
+    public static void customizeMiniMessage(@NotNull Consumer<MiniMessage.Builder> modifier) {
+      modifier.accept(builder);
+      modified = true;
     }
   }
 }
