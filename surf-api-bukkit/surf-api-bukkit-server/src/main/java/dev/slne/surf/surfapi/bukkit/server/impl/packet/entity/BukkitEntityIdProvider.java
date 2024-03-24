225e73ab84cce5dd1df2dc9d4af0d7a6e66acdec
twisti <twisti@slne.dev>
2024-03-24 20:29:34 +0100

    feat: replaced reflection with api method

--- surf-api-bukkit/surf-api-bukkit-server/src/main/java/dev/slne/surf/surfapi/bukkit/server/impl/packet/entity/BukkitEntityIdProvider.java
+++ surf-api-bukkit/surf-api-bukkit-server/src/main/java/dev/slne/surf/surfapi/bukkit/server/impl/packet/entity/BukkitEntityIdProvider.java
@@ -1,12 +1,13 @@
 package dev.slne.surf.surfapi.bukkit.server.impl.packet.entity;
 
-import dev.slne.surf.surfapi.bukkit.server.reflection.Reflection;
 import dev.slne.surf.surfapi.core.api.packet.entity.EntityIdProvider;
+import org.bukkit.Bukkit;
 
 public class BukkitEntityIdProvider implements EntityIdProvider {
 
   @Override
+  @SuppressWarnings("deprecation") // We are safe to use this method
   public int nextEntityId() {
-    return Reflection.ENTITY_PROXY.generateEntityId();
+    return Bukkit.getUnsafe().nextEntityId();
   }
 }
