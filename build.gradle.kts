9b18bedb9ec2add1fc5b6165c972d2bbe7105908
twisti <denizgebauer@web.de>
2024-06-20 15:24:31 +0200

    refactor: resolve merge error and downgrade minecraft version to 1.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("io.papermc.paperweight.userdev") version "1.7.1" apply false
     id("io.papermc.paperweight.core") version "1.7.1"
+    kotlin("jvm") version "2.0.0" apply false
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -11,7 +12,6 @@ paperweight {
     remapRepo = paperMavenPublicUrl
     decompileRepo = paperMavenPublicUrl
     minecraftVersion = "1.20.4"
-    kotlin("jvm") version "2.0.0" apply false
 }
 
 repositories {
@@ -20,10 +20,14 @@ repositories {
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    repositories {
+        mavenCentral()
+    }
 }
 
 dependencies {
-    paramMappings("net.fabricmc:yarn:1.20.6+build.1:mergedv2")
+    paramMappings("net.fabricmc:yarn:1.20.4+build.1:mergedv2")
     remapper("net.fabricmc:tiny-remapper:0.10.2:fat")
     decompiler("org.vineflower:vineflower:1.10.1")
 //    spigotDecompiler("io.papermc:patched-spigot-fernflower:0.1+build.13")
