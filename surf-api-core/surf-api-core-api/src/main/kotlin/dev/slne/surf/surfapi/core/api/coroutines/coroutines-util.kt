42e9ff52199f303f7807b3110d307c73c3129aee
twisti <denizgebauer@web.de>
2024-12-16 14:13:46 +0100

    refactor: remove old coroutines-util.kt class

--- surf-api-core/surf-api-core-api/src/main/kotlin/dev/slne/surf/surfapi/core/api/coroutines/coroutines-util.kt
+++
@@ -1,6 +0,0 @@
-package dev.slne.surf.surfapi.core.api.coroutines
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-
-fun defaultScope(block: suspend CoroutineScope.() -> Unit) = Dispatchers.Default
