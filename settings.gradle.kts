71aeface4fc515dbb9ee642352e559385303c27e
twisti <denizgebauer@web.de>
2024-12-20 22:55:47 +0100

    .

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,20 @@ pluginManagement {
     includeBuild("build-logic")
 }
 
+val ci = System.getenv("CI")?.toBoolean() == true
+
 rootProject.name = "surf-api"
 include(":surf-api-core:surf-api-core-api")
 include(":surf-api-core:surf-api-core-server")
 
 include(":surf-api-bukkit:surf-api-bukkit-api")
 include(":surf-api-bukkit:surf-api-bukkit-server")
-include(":surf-api-bukkit:surf-api-bukkit-plugin-test")
 
 include(":surf-api-velocity:surf-api-velocity-api")
 include(":surf-api-velocity:surf-api-velocity-server")
-include("surf-api-generator")
 include("surf-api-standalone")
+
+if (!ci) {
+    include(":surf-api-bukkit:surf-api-bukkit-plugin-test")
+    include("surf-api-generator")
+}
