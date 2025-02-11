96c5a835f9b630815034da4a22bfbb4787ab9fdb
twisti <denizgebauer@web.de>
2025-02-11 18:50:34 +0100

    feat: add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,34 @@
+import org.jetbrains.dokka.DokkaConfiguration
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.14" apply false
+    alias(libs.plugins.dokka)
 }
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    if (subprojects.isEmpty()) {
+        apply(plugin = rootProject.libs.plugins.dokka.get().pluginId)
+        println("Applying to $this")
+    }
+}
+
+tasks {
+    dokkaHtml {
+        outputDirectory.set(layout.buildDirectory.dir("documentation/html"))
+    }
+
+    withType<DokkaTask>().configureEach {
+        dokkaSourceSets.configureEach {
+            documentedVisibilities = setOf(
+                DokkaConfiguration.Visibility.PUBLIC,
+                DokkaConfiguration.Visibility.PROTECTED
+            )
+        }
+    }
+}
+
