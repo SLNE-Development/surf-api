a7117453851678a48ccde5f1ffdfbf8d393a4a7d
Ammo <ammo@slne.dev>
2025-02-15 05:41:50 +0100

    cleanup old repos

--- surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/surfapi/gradle/util/repo-extensions.kt
+++ surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/surfapi/gradle/util/repo-extensions.kt
@@ -3,8 +3,5 @@ package dev.slne.surf.surfapi.gradle.util
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.kotlin.dsl.maven
 
-fun RepositoryHandler.slneUnsafe() = maven("https://repo.slne.dev/repository/maven-unsafe/") { name = "maven-unsafe" }
 fun RepositoryHandler.slnePublic() = maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
-fun RepositoryHandler.slneSnapshots() = maven("https://repo.slne.dev/repository/maven-snapshots/") { name = "maven-snapshots" }
-fun RepositoryHandler.slneProxy() = maven("https://repo.slne.dev/repository/maven-proxy/") { name = "maven-proxy" }
-fun RepositoryHandler.slneExternalDevelopers() = maven("https://repo.slne.dev/repository/maven-external-developers/") { name = "maven-external-developers" }
+fun RepositoryHandler.slnePrivate() = maven("https://repo.slne.dev/repository/maven-private/") { name = "maven-private" }
