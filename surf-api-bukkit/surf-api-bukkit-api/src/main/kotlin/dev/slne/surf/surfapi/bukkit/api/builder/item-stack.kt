79d3a15f9e382d1a4fed5742bcf16542b4907269
twisti <denizgebauer@web.de>
2025-03-08 19:11:54 +0100

    feat: update version to 1.21.4-2.8.0-SNAPSHOT and refactor item stack DSL annotations

--- surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/builder/item-stack.kt
+++ surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/builder/item-stack.kt
@@ -1,26 +1,36 @@
 package dev.slne.surf.surfapi.bukkit.api.builder
 
+import dev.slne.surf.surfapi.core.api.messages.builder.SurfComponentBuilder
+import dev.slne.surf.surfapi.core.api.util.mutableObjectListOf
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
-@Target(AnnotationTarget.TYPE)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @DslMarker
-annotation class ItemMarker
+annotation class ItemDsl
 
 inline fun buildItem(
     material: Material,
     amount: Int = 1,
-    init: (@ItemMarker ItemStack).() -> Unit,
+    init: (@ItemDsl ItemStack).() -> Unit,
 ): ItemStack {
     val item = ItemStack(material, amount)
     item.init()
     return item
 }
 
-inline fun <reified M : ItemMeta> ItemStack.meta(block: (@ItemMarker M).() -> Unit): Boolean {
+inline fun ItemStack(
+    material: Material,
+    amount: Int = 1,
+    init: (@ItemDsl ItemStack).() -> Unit,
+): ItemStack {
+    return buildItem(material, amount, init)
+}
+
+inline fun <reified M : ItemMeta> ItemStack.meta(block: (@ItemDsl M).() -> Unit): Boolean {
     val meta = itemMeta as? M ?: return false
     meta.block()
     itemMeta = meta
@@ -28,7 +38,7 @@ inline fun <reified M : ItemMeta> ItemStack.meta(block: (@ItemMarker M).() -> Un
 }
 
 @JvmName("meta0")
-inline fun ItemStack.meta(init: (@ItemMarker ItemMeta).() -> Unit) {
+inline fun ItemStack.meta(init: (@ItemDsl ItemMeta).() -> Unit) {
     meta<ItemMeta>(init)
 }
 
@@ -42,4 +52,25 @@ fun ItemStack.lore(vararg lore: Component) {
     meta {
         lore(lore.map { it.decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE) })
     }
+}
+
+fun ItemStack.lore(block: @ItemDsl SurfComponentBuilder.() -> Unit) {
+    lore(SurfComponentBuilder().apply(block).build())
+}
+
+fun ItemStack.buildLore(block: @ItemDsl LoreBuilder.() -> Unit) {
+    val loreBuilder = LoreBuilder().apply(block)
+    lore(loreBuilder.build())
+}
+
+@ItemDsl
+class LoreBuilder {
+    private val lore = mutableObjectListOf<Component>()
+
+    fun line(block: @ItemDsl SurfComponentBuilder.() -> Unit) {
+        lore.add(SurfComponentBuilder().apply(block).build())
+    }
+
+    internal fun build() =
+        lore.map { it.decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE) }
 }
