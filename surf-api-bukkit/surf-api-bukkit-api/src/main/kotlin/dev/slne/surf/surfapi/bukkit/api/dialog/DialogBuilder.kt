e3c7ba5db6a05f112997b3b226564c44cda45b37
twisti <denizgebauer@web.de>
2025-07-06 13:33:21 +0200

    feat: add clearDialogs function to Player and implement in NmsCommonBridge

--- surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/dialog/DialogBuilder.kt
+++ surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/dialog/DialogBuilder.kt
@@ -6,8 +6,11 @@ import dev.slne.surf.surfapi.bukkit.api.dialog.builder.DialogBase
 import dev.slne.surf.surfapi.bukkit.api.dialog.builder.DialogBaseBuilder
 import dev.slne.surf.surfapi.bukkit.api.dialog.builder.DialogType
 import dev.slne.surf.surfapi.bukkit.api.dialog.builder.DialogTypeBuilder
+import dev.slne.surf.surfapi.bukkit.api.nms.NmsUseWithCaution
+import dev.slne.surf.surfapi.bukkit.api.nms.bridges.nmsCommonBridge
 import io.papermc.paper.dialog.Dialog
 import io.papermc.paper.registry.data.dialog.DialogRegistryEntry
+import org.bukkit.entity.Player
 
 fun dialog(builder: DialogRegistryEntry.Builder.() -> Unit): Dialog =
     Dialog.create { it.empty().apply(builder) }
@@ -21,4 +24,9 @@ fun DialogRegistryEntry.Builder.base(block: DialogBaseBuilder.() -> Unit) {
 
 fun DialogRegistryEntry.Builder.type(block: DialogTypeBuilder.() -> Unit) {
     type(DialogType(block))
+}
+
+@NmsUseWithCaution
+fun Player.clearDialogs() {
+    nmsCommonBridge.clearDialogs(this)
 }
