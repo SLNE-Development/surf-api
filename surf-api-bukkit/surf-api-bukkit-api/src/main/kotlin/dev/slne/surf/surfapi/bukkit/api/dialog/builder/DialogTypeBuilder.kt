6f9751c0f8cf0ffccc5dd4384852d8e8b9b1dff9
twisti <denizgebauer@web.de>
2025-07-07 20:05:09 +0200

    feat: enhance DialogListTypeBuilder to support dynamic dialog addition and improve dialog management

--- surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/dialog/builder/DialogTypeBuilder.kt
+++ surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/dialog/builder/DialogTypeBuilder.kt
@@ -3,7 +3,9 @@
 package dev.slne.surf.surfapi.bukkit.api.dialog.builder
 
 import dev.slne.surf.surfapi.core.api.util.mutableObjectListOf
+import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
 import io.papermc.paper.dialog.Dialog
+import io.papermc.paper.registry.RegistryAccess
 import io.papermc.paper.registry.RegistryKey
 import io.papermc.paper.registry.TypedKey
 import io.papermc.paper.registry.data.dialog.ActionButton
@@ -27,16 +29,22 @@ class DialogTypeBuilder {
         type = DialogType.notice(ActionButton(block))
     }
 
-    fun dialogList(dialogs: RegistrySet<Dialog>, block: DialogListTypeBuilder.() -> Unit = {}) {
-        type = DialogListTypeBuilder(dialogs).apply(block).build()
-    }
-
-    fun dialogList(vararg key: TypedKey<Dialog>, block: DialogListTypeBuilder.() -> Unit = {}) {
-        dialogList(RegistrySet.keySet(RegistryKey.DIALOG, *key), block)
+    fun dialogList(vararg keys: TypedKey<Dialog>, block: DialogListTypeBuilder.() -> Unit = {}) {
+        dialogList {
+            dialog(*keys)
+            block()
+        }
     }
 
     fun dialogList(vararg dialogs: Dialog, block: DialogListTypeBuilder.() -> Unit = {}) {
-        dialogList(RegistrySet.valueSet(RegistryKey.DIALOG, dialogs.toList()), block)
+        dialogList {
+            addAll(*dialogs)
+            block()
+        }
+    }
+
+    fun dialogList(block: DialogListTypeBuilder.() -> Unit) {
+        type = DialogListTypeBuilder().apply(block).build()
     }
 
     fun confirmation(block: DialogConfirmationTypeBuilder.() -> Unit = {}) {
@@ -61,7 +69,10 @@ class DialogTypeBuilder {
         }.build()
     }
 
-    fun multiAction(vararg actions: ActionButton, block: DialogMultiActionTypeBuilder.() -> Unit = {}) {
+    fun multiAction(
+        vararg actions: ActionButton,
+        block: DialogMultiActionTypeBuilder.() -> Unit = {},
+    ) {
         multiAction(actions.toList(), block)
     }
 
@@ -75,10 +86,11 @@ class DialogTypeBuilder {
         return type
     }
 
-    class DialogListTypeBuilder(private val dialogs: RegistrySet<Dialog>) {
+    class DialogListTypeBuilder {
         var exitAction: ActionButton? = null
         var columns: @Range(from = 1, to = Int.MAX_VALUE.toLong()) Int? = null
         var buttonWidth: @Range(from = 1, to = 1024) Int? = null
+        private val dialogs = mutableObjectSetOf<Dialog>()
 
         fun exitAction(exitAction: ActionButton) {
             this.exitAction = exitAction
@@ -96,8 +108,32 @@ class DialogTypeBuilder {
             this.buttonWidth = buttonWidth
         }
 
+        fun dialog(dialog: Dialog) {
+            dialogs.add(dialog)
+        }
+
+        fun addAll(vararg dialogs: Dialog) {
+            this.dialogs.addAll(dialogs)
+        }
+
+        fun addAll(dialogs: Iterable<Dialog>) {
+            this.dialogs.addAll(dialogs)
+        }
+
+        fun dialog(key: TypedKey<Dialog>) {
+            dialogs.add(
+                RegistryAccess.registryAccess().getRegistry(RegistryKey.DIALOG).getOrThrow(key)
+            )
+        }
+
+        fun dialog(vararg keys: TypedKey<Dialog>) {
+            dialogs.addAll(
+                keys.map { RegistryAccess.registryAccess().getRegistry(RegistryKey.DIALOG).getOrThrow(it) }
+            )
+        }
+
         internal fun build(): DialogType {
-            val builder = DialogType.dialogList(dialogs)
+            val builder = DialogType.dialogList(RegistrySet.valueSet(RegistryKey.DIALOG, dialogs))
             with(builder) {
                 exitAction?.let { exitAction(it) }
                 columns?.let { columns(it) }
