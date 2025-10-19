effcbe34f3fe45f9bd2b0a60a84f42cc1574c3d3
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-19 11:41:55 +0200

    feat: refactor toast implementation to streamline packet creation and enhance clarity

--- surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/impl/toast/ToastImpl.kt
+++ surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/impl/toast/ToastImpl.kt
@@ -14,6 +14,8 @@ import dev.slne.surf.surfapi.core.api.util.mutableObject2ObjectMapOf
 import net.kyori.adventure.text.Component
 import java.util.*
 
+private const val CRITERION_ID = "surfapi_toast"
+
 class ToastImpl(
     override val icon: ItemType,
     override val text: Component,
@@ -23,60 +25,10 @@ class ToastImpl(
         val user =
             PacketEvents.getAPI().playerManager.getUser(surfCoreApi.getPlayer(player) ?: return)
                 ?: return
-        val stack =
-            ItemStack.builder().type(icon).build()
-        val criterionName = "surfapi_toast"
-        val criterion: List<String> = Collections.singletonList(criterionName)
         val resourceLocation = ResourceLocation.minecraft("surfapi_toast_${UUID.randomUUID()}")
-        val advancement = Advancement(
-            null,
-            AdvancementDisplay(
-                text,
-                buildText {
-                    error("test")
-                },
-                stack,
-                style.toType(),
-                null,
-                true,
-                false,
-                0.0f,
-                0.0f
-            ),
-            criterion,
-            Collections.singletonList(criterion),
-            false
-        )
 
-        val showPacket = WrapperPlayServerUpdateAdvancements(
-            false,
-            Collections.singletonList(
-                AdvancementHolder(
-                    resourceLocation,
-                    advancement
-                )
-            ),
-            Collections.emptySet(),
-            Collections.singletonMap(
-                resourceLocation, AdvancementProgress(
-                    mutableObject2ObjectMapOf(
-                        criterionName to AdvancementProgress.CriterionProgress(System.currentTimeMillis())
-                    )
-                )
-            ),
-            true
-        )
-
-        val hidePacket = WrapperPlayServerUpdateAdvancements(
-            false,
-            Collections.emptyList(),
-            Collections.singleton(resourceLocation),
-            Collections.emptyMap(),
-            false
-        )
-
-        user.sendPacket(showPacket)
-        user.sendPacket(hidePacket)
+        user.sendPacket(buildShowPacket(resourceLocation, this))
+        user.sendPacket(buildHidePacket(resourceLocation))
     }
 }
 
@@ -85,3 +37,55 @@ private fun ToastStyle.toType() = when (this) {
     ToastStyle.GOAL -> AdvancementType.GOAL
     ToastStyle.CHALLENGE -> AdvancementType.CHALLENGE
 }
+
+private fun createFakeAdvancement(toast: Toast): Advancement {
+    val criterion: List<String> = Collections.singletonList(CRITERION_ID)
+    return Advancement(
+        null,
+        AdvancementDisplay(
+            toast.text,
+            buildText {
+                error("test")
+            },
+            ItemStack.builder().type(toast.icon).build(),
+            toast.style.toType(),
+            null,
+            true,
+            false,
+            0.0f,
+            0.0f
+        ),
+        criterion,
+        Collections.singletonList(criterion),
+        false
+    )
+}
+
+private fun buildShowPacket(resourceLocation: ResourceLocation, toast: Toast) =
+    WrapperPlayServerUpdateAdvancements(
+        false,
+        Collections.singletonList(
+            AdvancementHolder(
+                resourceLocation,
+                createFakeAdvancement(toast)
+            )
+        ),
+        Collections.emptySet(),
+        Collections.singletonMap(
+            resourceLocation, AdvancementProgress(
+                mutableObject2ObjectMapOf(
+                    CRITERION_ID to AdvancementProgress.CriterionProgress(System.currentTimeMillis())
+                )
+            )
+        ),
+        true
+    )
+
+private fun buildHidePacket(resourceLocation: ResourceLocation) =
+    WrapperPlayServerUpdateAdvancements(
+        false,
+        Collections.emptyList(),
+        Collections.singleton(resourceLocation),
+        Collections.emptyMap(),
+        false
+    )
