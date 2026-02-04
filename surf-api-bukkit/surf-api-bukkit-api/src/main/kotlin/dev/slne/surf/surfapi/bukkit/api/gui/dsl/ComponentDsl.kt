4e911c1d4077e5058972092a33b0f50bf61d9f1c
Ammo <ammo@slne.dev>
2026-02-04 01:26:15 +0100

    refactor: simplify attachedRef assignment in ComponentDsl for improved type handling

--- surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/gui/dsl/ComponentDsl.kt
+++ surf-api-bukkit/surf-api-bukkit-api/src/main/kotlin/dev/slne/surf/surfapi/bukkit/api/gui/dsl/ComponentDsl.kt
@@ -57,7 +57,7 @@ class ComponentBuilder {
             }
         }.also { component ->
             ref?.set(component)
-            component.attachedRef = ref as? Ref<Component>
+            component.attachedRef = ref
         }
     }
 }
