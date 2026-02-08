b8aae658ed9e35cf3e7d026480a60dcfedb44879
twisti <denizgebauer@web.de>
2026-02-08 14:13:17 +0100

    fix: change output property from directory to file in GeneratePluginFile

--- surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/surfapi/gradle/generators/GeneratePluginFile.kt
+++ surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/surfapi/gradle/generators/GeneratePluginFile.kt
@@ -7,7 +7,7 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 @CacheableTask
@@ -18,7 +18,7 @@ abstract class GeneratePluginFile : DefaultTask() {
     @get:Input
     abstract val pluginFileJson: Property<String>
 
-    @get:OutputDirectory
+    @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
     @TaskAction
