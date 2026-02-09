a659f1f9c72dc7e946160318d65fce359894678f
twisti <denizgebauer@web.de>
2026-02-09 17:37:29 +0100

    refactor: add click callback extension functions for Audience handling

---
+++ surf-api-core/surf-api-core-api/src/main/kotlin/dev/slne/surf/surfapi/core/api/messages/adventure/callback-extension.kt
@@ -0,0 +1,71 @@
+package dev.slne.surf.surfapi.core.api.messages.adventure
+
+import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.BuildableComponent
+import net.kyori.adventure.text.ComponentBuilder
+import net.kyori.adventure.text.event.ClickCallback
+import net.kyori.adventure.text.event.ClickEvent
+import java.util.function.Consumer
+import kotlin.experimental.ExperimentalTypeInference
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
+
+fun <C : BuildableComponent<C, B>, B : ComponentBuilder<C, B>> ComponentBuilder<C, B>.clickCallback(
+    callback: ClickCallback<Audience>,
+) = clickEvent(ClickEvent.callback(callback))
+
+fun <C : BuildableComponent<C, B>, B : ComponentBuilder<C, B>> ComponentBuilder<C, B>.clickCallbackWithOptions(
+    builder: ClickCallbackWithOptionsBuilder<Audience>.() -> Unit,
+) = clickEvent(ClickCallbackWithOptionsBuilder(Audience::class.java).apply(builder).build())
+
+inline fun <reified T : Audience, C : BuildableComponent<C, B>, B : ComponentBuilder<C, B>> ComponentBuilder<C, B>.clickCallbackTyped(
+    callback: ClickCallback<T>,
+) = clickEvent(ClickEvent.callback(ClickCallback.widen(callback, T::class.java)))
+
+@OptIn(ExperimentalTypeInference::class)
+inline fun <reified T : Audience, C : BuildableComponent<C, B>, B : ComponentBuilder<C, B>> ComponentBuilder<C, B>.clickCallbackTypedWithOptions(
+    @ClickCallbackWithOptionsBuilderDsl @BuilderInference builder: ClickCallbackWithOptionsBuilder<T>.() -> Unit
+) = clickEvent(ClickCallbackWithOptionsBuilder(T::class.java).apply(builder).build())
+
+fun ClickCallback.Options.Builder.lifetime(duration: Duration) = lifetime(duration.toJavaDuration())
+
+@DslMarker
+annotation class ClickCallbackWithOptionsBuilderDsl
+
+@ClickCallbackWithOptionsBuilderDsl
+class ClickCallbackWithOptionsBuilder<T : Audience> @PublishedApi internal constructor(private val type: Class<T>) {
+    private var callback: ClickCallback<T>? = null
+    private var permission: String? = null
+    private var permissionOtherwise: Consumer<in Audience>? = null
+    private var options: ClickCallback.Options = ClickCallback.Options.builder().build()
+
+    fun requiresPermission(permission: String) {
+        this.permission = permission
+    }
+
+    fun requiresPermissionOrElse(
+        permission: String,
+        @ClickCallbackWithOptionsBuilderDsl callback: Consumer<in Audience>
+    ) {
+        this.permission = permission
+        this.permissionOtherwise = callback
+    }
+
+    fun callback(@ClickCallbackWithOptionsBuilderDsl callback: ClickCallback<T>) {
+        this.callback = callback
+    }
+
+    fun options(options: ClickCallback.Options.Builder.() -> Unit) {
+        this.options = ClickCallback.Options.builder(this.options).apply(options).build()
+    }
+
+    @PublishedApi
+    internal fun build(): ClickEvent {
+        val callback = callback ?: return ClickEvent.callback { }
+        val callbackWithPermission = permission
+            ?.let { permission -> callback.requiringPermission(permission, permissionOtherwise) }
+            ?: callback
+
+        return ClickEvent.callback(ClickCallback.widen(callbackWithPermission, type), options)
+    }
+}
