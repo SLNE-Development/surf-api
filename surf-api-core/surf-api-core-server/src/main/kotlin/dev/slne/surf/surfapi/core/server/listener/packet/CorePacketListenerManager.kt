70c14df7a01189cf152f415c5ad16f4c6e941b00
twisti <denizgebauer@web.de>
2026-02-10 20:22:07 +0100

    refactor: update CorePacketListenerManager to use packetEvents for event management

--- surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/listener/packet/CorePacketListenerManager.kt
+++ surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/listener/packet/CorePacketListenerManager.kt
@@ -1,24 +1,24 @@
 package dev.slne.surf.surfapi.core.server.listener.packet
 
-import com.github.retrooper.packetevents.PacketEvents
 import com.github.retrooper.packetevents.event.PacketListenerCommon
-import com.github.retrooper.packetevents.event.PacketListenerPriority
+import dev.slne.surf.surfapi.core.api.extensions.packetEvents
 import dev.slne.surf.surfapi.core.api.util.objectSetOf
-import dev.slne.surf.surfapi.core.server.listener.packet.chat.ChatPacketListener
 
 object CorePacketListenerManager {
-    private val listeners = objectSetOf(ChatPacketListener(PacketListenerPriority.NORMAL))
+    private val listeners = objectSetOf<PacketListenerCommon>(
+//        ChatPacketListener(PacketListenerPriority.NORMAL)
+    )
 
     fun registerListeners() {
         listeners.forEach { register(it) }
     }
 
     fun unregisterListeners() {
-        val eventManager = PacketEvents.getAPI().eventManager
+        val eventManager = packetEvents.eventManager
         listeners.forEach { eventManager.unregisterListener(it) }
     }
 
     private fun register(listener: PacketListenerCommon) {
-        PacketEvents.getAPI().eventManager.registerListener(listener)
+        packetEvents.eventManager.registerListener(listener)
     }
 }
