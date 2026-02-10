8b3b8ef794a989ba17c679a5b26a02e558f553cf
twisti <denizgebauer@web.de>
2026-02-10 20:16:56 +0100

    refactor: simplify toString method in SurfCoreApiImpl for clarity

--- surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/impl/SurfCoreApiImpl.kt
+++ surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/surfapi/core/server/impl/SurfCoreApiImpl.kt
@@ -1,7 +1,6 @@
 package dev.slne.surf.surfapi.core.server.impl
 
 import dev.slne.surf.surfapi.core.api.SurfCoreApi
-import org.apache.commons.lang3.builder.ToStringBuilder
 
 /**
  * The SurfCoreApiImpl class is an implementation of the SurfCoreApi interface. It provides the
@@ -10,6 +9,6 @@ import org.apache.commons.lang3.builder.ToStringBuilder
 abstract class SurfCoreApiImpl protected constructor() : SurfCoreApi {
 
     override fun toString(): String {
-        return ToStringBuilder.reflectionToString(this)
+        return "SurfCoreApiImpl()"
     }
 }
