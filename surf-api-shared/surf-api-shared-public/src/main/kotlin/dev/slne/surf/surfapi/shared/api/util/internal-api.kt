e3aa09c53b57b05ca5642b6637fda7fee5d5ddc1
twisti <denizgebauer@web.de>
2026-03-05 16:29:57 +0100

    Add InternalAPIMarker to InternalSurfApi

--- surf-api-shared/surf-api-shared-public/src/main/kotlin/dev/slne/surf/surfapi/shared/api/util/internal-api.kt
+++ surf-api-shared/surf-api-shared-public/src/main/kotlin/dev/slne/surf/surfapi/shared/api/util/internal-api.kt
@@ -1,7 +1,10 @@
 package dev.slne.surf.surfapi.shared.api.util
 
+import dev.slne.surf.surfapi.shared.api.annotation.InternalAPIMarker
+
 @RequiresOptIn(
     "This API is internal and should not be used from outside the library",
     RequiresOptIn.Level.ERROR
 )
+@InternalAPIMarker
 annotation class InternalSurfApi
