ca6370652d609fc7bc3e9db9b5f91dc52ae6256c
Ammo <ammo@slne.dev>
2026-04-04 01:53:10 +0200

    refactored standalone

--- surf-api-standalone/src/main/kotlin/dev/slne/surf/surfapi/standalone/impl/SurfStandaloneInstance.kt
+++ surf-api-standalone/src/main/kotlin/dev/slne/surf/surfapi/standalone/impl/SurfStandaloneInstance.kt
@@ -2,5 +2,4 @@ package dev.slne.surf.surfapi.standalone.impl
 
 import dev.slne.surf.surfapi.core.server.CoreInstance
 
-object SurfStandaloneInstance: CoreInstance() {
-}
+object SurfStandaloneInstance : CoreInstance()
