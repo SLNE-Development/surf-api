960c99adb2c5e75be884b990cbbbd7cf1ed0dcba
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-04-26 11:23:44 +0200

    ♻️ refactor(event-bus): add newline at end of SurfAsyncEventInvoker.kt
    - add a newline at the end of SurfAsyncEventInvoker.kt for consistency with coding standards

--- surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/api/core/server/impl/event/invoker/SurfAsyncEventInvoker.kt
+++ surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/api/core/server/impl/event/invoker/SurfAsyncEventInvoker.kt
@@ -3,5 +3,6 @@ package dev.slne.surf.api.core.server.impl.event.invoker
 import dev.slne.surf.api.core.event.SurfAsyncEvent
 
 fun interface SurfAsyncEventInvoker {
+
     suspend fun invoke(event: SurfAsyncEvent)
 }
