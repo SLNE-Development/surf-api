fb2d9e6f3c1a6efbd2bdc8a86817fff22f032e12
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-04-26 13:42:10 +0200

    ✨ feat(event-bus): add MethodHandles.Lookup to SurfEventInvokerFactory for dynamic method invocation

--- surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/api/core/server/impl/event/invoker/SurfEventInvokerFactory.kt
+++ surf-api-core/surf-api-core-server/src/main/kotlin/dev/slne/surf/api/core/server/impl/event/invoker/SurfEventInvokerFactory.kt
@@ -2,16 +2,21 @@ package dev.slne.surf.api.core.server.impl.event.invoker
 
 import dev.slne.surf.api.core.invoker.InvokerFactory
 import dev.slne.surf.api.shared.api.util.InternalInvokerApi
+import java.lang.invoke.MethodHandles
 
 @OptIn(InternalInvokerApi::class)
 object SurfEventInvokerFactory {
+    val lookup: MethodHandles.Lookup = MethodHandles.lookup()
+
     val syncFactory: InvokerFactory<SurfSyncEventInvoker> = InvokerFactory(
         SurfSyncEventInvokerTemplate::class.java,
-        SurfSyncEventInvoker::class.java
+        SurfSyncEventInvoker::class.java,
+        lookup
     )
 
     val asyncFactory: InvokerFactory<SurfAsyncEventInvoker> = InvokerFactory(
         SurfAsyncEventInvokerTemplate::class.java,
         SurfAsyncEventInvoker::class.java,
+        lookup
     )
 }
