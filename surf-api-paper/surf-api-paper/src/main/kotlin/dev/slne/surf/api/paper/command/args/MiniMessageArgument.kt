d0f5cc195769273dd26cc546f6097792a2ad3581
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-07-01 12:16:06 +0200

    ✨ feat(minimessage): add greedy argument support to MiniMessageArgument
    - introduce greedy parameter to MiniMessageArgument constructor
    - update miniMessageArgument function to accept greedy argument
    - change default command exception message prefix to Colors.ERROR_PREFIX
    - increment version to 3.32.0 in gradle.properties

--- surf-api-paper/surf-api-paper/src/main/kotlin/dev/slne/surf/api/paper/command/args/MiniMessageArgument.kt
+++ surf-api-paper/surf-api-paper/src/main/kotlin/dev/slne/surf/api/paper/command/args/MiniMessageArgument.kt
@@ -1,21 +1,28 @@
+@file:OptIn(ExperimentalVersionOverloading::class)
+
 package dev.slne.surf.api.paper.command.args
 
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.CommandTree
 import dev.jorel.commandapi.arguments.Argument
 import dev.jorel.commandapi.arguments.CustomArgument
+import dev.jorel.commandapi.arguments.GreedyStringArgument
 import dev.jorel.commandapi.arguments.TextArgument
 import dev.slne.surf.api.core.command.builder.CommandExceptionBuilder
+import dev.slne.surf.api.core.minimessage.miniMessage
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.minimessage.ParsingException
 
-open class MiniMessageArgument(nodeName: String) : CustomArgument<Component, String>(
-    TextArgument(nodeName),
+open class MiniMessageArgument(
+    nodeName: String,
+    @IntroducedAt("3.32.0") greedy: Boolean = false,
+) : CustomArgument<Component, String>(
+    if (greedy) GreedyStringArgument(nodeName) else TextArgument(nodeName),
     { info ->
         val raw = info.currentInput
 
         try {
-            MiniMessage.miniMessage().deserialize(raw)
+            miniMessage.deserialize(raw)
         } catch (e: ParsingException) {
             throw CustomArgumentException.fromAdventureComponent(
                 CommandExceptionBuilder(
@@ -31,6 +38,13 @@ open class MiniMessageArgument(nodeName: String) : CustomArgument<Component, Str
 inline fun CommandAPICommand.miniMessageArgument(
     nodeName: String,
     optional: Boolean = false,
+    @IntroducedAt("3.32.0") greedy: Boolean = false,
     block: Argument<*>.() -> Unit = {},
-): CommandAPICommand =
-    withArguments(MiniMessageArgument(nodeName).setOptional(optional).apply(block))
+): CommandAPICommand = withArguments(MiniMessageArgument(nodeName, greedy).setOptional(optional).apply(block))
+
+inline fun CommandTree.miniMessageArgument(
+    nodeName: String,
+    optional: Boolean = false,
+    @IntroducedAt("3.32.0") greedy: Boolean = false,
+    block: Argument<*>.() -> Unit = {}
+): CommandTree = then(MiniMessageArgument(nodeName, greedy).setOptional(optional).apply(block))
