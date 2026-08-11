04a7aed096fee7e20680be81a6b67ae1277f30a9
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-08-11 16:44:20 +0200

    ✨ feat(core): update version to 2.1.3 and add Minestom support
    - increment version from 2.1.2 to 2.1.3 in build.gradle.kts
    - add withCoreMinestom function to CoreSurfExtension.kt
    - introduce MINESTOM module in SurfCoreModules.kt

--- surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/api/gradle/SurfCoreModules.kt
+++ surf-api-gradle-plugin/src/main/kotlin/dev/slne/surf/api/gradle/SurfCoreModules.kt
@@ -4,4 +4,5 @@ internal enum class SurfCoreModules(val module: String) {
     COMMON("surf-core-api-common"),
     PAPER("surf-core-api-paper"),
     VELOCITY("surf-core-api-velocity"),
+    MINESTOM("surf-core-api-minestom"),
 }
